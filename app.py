0ba716650c892d8174c8aecf3f9923f0ea347c70
Jhonjo26 <juanjovoy23@gmail.com>
2026-05-20 06:39:55 +0000

    cambio

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import streamlit as st
+
+mlit as st
 from main import clasificar_humedad
 import datetime
 
@@ -322,7 +323,7 @@ with col1:
 with col2:
     telefono = st.text_input("Telefono", placeholder="Ej: 11-4567-8900")
 direccion = st.text_input("Direccion del inmueble", placeholder="Ej: Av. Corrientes 1234, CABA")
-
+st.write("📋 **Texto de Bienvenida:** Bienvenidos a un sistema funcional de soluciones para analizar, diagnosticar y lograr la erradicación definitiva de la humedad en casas, comercios y edificios.")
 st.markdown('<div class="seccion-label">Descripcion del problema</div>', unsafe_allow_html=True)
 descripcion = st.text_area(
     "Describi el problema con tus palabras",
